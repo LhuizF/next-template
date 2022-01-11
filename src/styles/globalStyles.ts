2cab98c64e4abf0e5c94510a5274ac2b200893b4
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-01-10 21:41:48 -0300

    Initial Commit

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+    body{
+    background-color: #D8D8D8;
+
+    h1 {
+      font-family: Roboto;
+      text-align: center;
+      font-weight: 700;
+    }
+
+  }
+`;
